3c077c1410e2505505f723d96d51b59f507614a9
ghaschel <ghaschel@gmail.com>
2018-03-15 23:05:15 -0300

    Made userscript domain, match and url global

--- userscript.js
+++ userscript.js
@@ -1,15 +1,15 @@
 // ==UserScript==
-// @name         HipChat4k
-// @domain       sinaxti.hipchat.com
+// @name         HipChat HD
+// @domain       *.hipchat.com
 // @version      0.1
 // @description  Make emoticons bigger!
 // @author       Guilherme Haschel
-// @icon         https://bytebucket.org/echo__off/hipchat4k/raw/65e6aba901bb94f0107c3d6f8ba82df8906c4c83/hd-48.png
-// @icon64       https://bytebucket.org/echo__off/hipchat4k/raw/65e6aba901bb94f0107c3d6f8ba82df8906c4c83/hd-64.png
-// @include      https://sinaxti.hipchat.com/chat/*
-// @match        https://sinaxti.hipchat.com/chat/*
-// @updateURL    https://bitbucket.org/echo__off/hipchat4k/raw/42c016b8ef9c714e8fec15eabf813ed4408be088/userscript.js
-// @downloadURL  https://bitbucket.org/echo__off/hipchat4k/raw/42c016b8ef9c714e8fec15eabf813ed4408be088/userscript.js
+// @icon         https://raw.githubusercontent.com/ghaschel/hipchat-hd/master/hd-48.png
+// @icon64       https://raw.githubusercontent.com/ghaschel/hipchat-hd/master/hd-64.png
+// @include      https://*.hipchat.com/chat/*
+// @match        https://*.hipchat.com/chat/*
+// @updateURL    https://raw.githubusercontent.com/ghaschel/hipchat-hd/master/userscript.js
+// @downloadURL  https://raw.githubusercontent.com/ghaschel/hipchat-hd/master/userscript.js
 // @grant        none
 // @license      MIT
 // ==/UserScript==
