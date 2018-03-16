b3134f68604124777edb75188f412b495610cedf
ghaschel <ghaschel@gmail.com>
2018-03-16 12:39:14 -0300

    Improved code, removed jquery references, made smileys @2x instead of @4x

--- userscript.js
+++ userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         HipChat HD
 // @domain       *.hipchat.com
-// @version      0.1
+// @version      0.2
 // @description  Make emoticons bigger!
 // @author       Guilherme Haschel
 // @icon         https://raw.githubusercontent.com/ghaschel/hipchat-hd/master/hd-48.png
@@ -16,19 +16,42 @@
 
 (function() {
     'use strict';
+
+    var sheet = (function() {
+        var style = document.createElement("style");
+        style.appendChild(document.createTextNode(""));
+        document.head.appendChild(style);
+        return style.sheet;
+    })();
+
+    sheet.insertRule("#hipchat .hc-tooltip .smileys-selection, body .hc-tooltip .smileys-selection { width: 184px; }", sheet.length);
+
     var makeItHD = function(){
-        $('img.remoticon').each(function(idx, el){
-            var bkp = el.src;
-            bkp = bkp.replace('@2x', '@4x');
-            el.src = bkp;
+        var smileys = document.querySelectorAll('.smiley-icon > .hc-tooltip  img.remoticon:not(.changed)');
+        var emoticons = document.querySelectorAll('.hc-chat-scrollbox.message-list img.remoticon:not(.changed)');
+
+        emoticons.forEach(function(el, idx){
+            el.src = el.src.replace('@2x', '@4x');;
+            el.classList.add('changed');
             el.removeAttribute('height');
             el.removeAttribute('width');
+            console.log(el);
+        });
+
+        smileys.forEach(function(el, idx) {
+            el.classList.add('changed');
+            el.attributes.height.value = 40;
+            el.attributes.width.value = 40;
         });
     };
-    $('.hc-main-col').bind('DOMSubtreeModified', function(){
-        var isChat = $('header.aui-page-header.lobby-header').length === 0;
+
+    new MutationObserver(function(mutations) {
+        var isChat = document.querySelectorAll('header.aui-page-header.lobby-header').length === 0;
         if (isChat) {
             makeItHD();
         }
-    });
+    }).observe(
+        document.querySelector('.hc-main-col'),
+        { subtree: true, childList: true }
+    );
 })();
